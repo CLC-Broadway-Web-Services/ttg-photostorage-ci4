35a78753186ab608518ad9aacd7c263954e2624c
Ahtesham <ahtesham2000@gmail.com>
2022-01-07 18:09:34 +0530

    updates

--- app/Views/Dashboard/layout.php
+++ app/Views/Dashboard/layout.php
@@ -8,12 +8,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="A powerful and conceptual apps base dashboard template that especially build for developers and programmers.">
     <!-- Fav Icon  -->
-    <link rel="shortcut icon" href="./images/favicon.png">
+    <link rel="shortcut icon" href="/public/images/jsTree/TTG-Photo-Storage-Favicon.png">
     <!-- Page Title  -->
     <title>TTG-PHOTOSTORAGE</title>
     <!-- StyleSheets  -->
-    <link rel="stylesheet" href="/public/css/dashlite.css?ver=2.4.0">
-    <link id="skin-default" rel="stylesheet" href="/public/css/theme.css?ver=2.4.0">
+    <link rel="stylesheet" href="/public/css/dashlite.css">
+    <link id="skin-default" rel="stylesheet" href="/public/css/theme.css">
 </head>
 
 <body class="nk-body bg-lighter npc-default has-sidebar ">
@@ -25,9 +25,9 @@
                 <div class="nk-sidebar-element nk-sidebar-head">
                     <div class="nk-sidebar-brand">
                         <a href="html/index.html" class="logo-link nk-sidebar-logo">
-                            <img class="logo-light logo-img" src="/public/images/jsTree/TTG-Photo Storage-Logos.png" srcset="./images/logo2x.png 2x" alt="logo">
-                            <img class="logo-dark logo-img" src="/public/images/jsTree/TTG-Photo Storage-Logos.png" srcset="./images/logo-dark2x.png 2x" alt="logo-dark">
-                            <img class="logo-small logo-img logo-img-small" src="/public/images/jsTree/TTG-Photo Storage-Favicon.png" srcset="./images/logo-small2x.png 2x" alt="logo-small">
+                            <img class="logo-light logo-img" src="/public/images/jsTree/TTG-Photo-Storage-Logos.png" srcset="/public/images/jsTree/TTG-Photo-Storage-Logos.png 2x" alt="logo">
+                            <img class="logo-dark logo-img" src="/public/images/jsTree/TTG-Photo-Storage-Logos.png" srcset="/public/images/jsTree/TTG-Photo-Storage-Logos.png 2x" alt="logo-dark">
+                            <img class="logo-small logo-img logo-img-small" src="/public/images/jsTree/TTG-Photo-Storage-Favicon.png" srcset="/public/images/jsTree/TTG-Photo-Storage-Favicon.png 2x" alt="logo-small">
                         </a>
                     </div>
                     <div class="nk-menu-trigger mr-n2">
@@ -126,8 +126,8 @@
                             </div>
                             <div class="nk-header-brand d-xl-none">
                                 <a href="html/index.html" class="logo-link">
-                                    <img class="logo-light logo-img" src="./images/logo.png" srcset="./images/logo2x.png 2x" alt="logo">
-                                    <img class="logo-dark logo-img" src="./images/logo-dark.png" srcset="./images/logo-dark2x.png 2x" alt="logo-dark">
+                                    <img class="logo-light logo-img" src="/public/images/jsTree/TTG-Photo-Storage-Logos.png" srcset="/public/images/jsTree/TTG-Photo-Storage-Logos.png 2x" alt="logo">
+                                    <img class="logo-dark logo-img" src="/public/images/jsTree/TTG-Photo-Storage-Logos.png" srcset="/public/images/jsTree/TTG-Photo-Storage-Logos.png 2x" alt="logo-dark">
                                 </a>
                             </div>
                             
@@ -285,12 +285,12 @@
     </div>
     <!-- app-root @e -->
     <!-- JavaScript -->
-    <script src="/public/js/bundle.js?ver=2.4.0"></script>
-    <script src="/public/js/scripts.js?ver=2.4.0"></script>
-    <script src="/public/js/charts/chart-analytics.js?ver=2.4.0"></script>
-    <script src="/public/js/libs/jqvmap.js?ver=2.4.0"></script>
-    <script src="/public/js/charts/chart-ecommerce.js?ver=2.4.0"></script>
-    <script src="/public/js/charts/chart-sales.js?ver=2.4.0"></script>
+    <script src="/public/js/bundle.js"></script>
+    <script src="/public/js/scripts.js"></script>
+    <!-- <script src="/public/js/charts/chart-analytics.js"></script> -->
+    <!-- <script src="/public/js/libs/jqvmap.js"></script> -->
+    <!-- <script src="/public/js/charts/chart-ecommerce.js"></script> -->
+    <!-- <script src="/public/js/charts/chart-sales.js"></script> -->
     <?= $this->renderSection('javascript') ?>
 </body>
 
